a2f58a0bae6678a486e7cbbdc21db66c2046d730
Dmitriy <dima2394@mail.ru>
2022-03-16 15:09:10 +0300

    initial
    3 version

--- Package.swift
+++ Package.swift
@@ -10,7 +10,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftyFrame",
-            targets: ["SwiftyFrame"]),
+            targets: ["SwiftyFrame"],
+            path: "Sources/SwiftyFrame")
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
